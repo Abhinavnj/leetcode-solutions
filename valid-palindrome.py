eb6a352e1a1d0bb7648f7baecba2e9978343f023
Abhinav Sirohi <abhinavnj@gmail.com>
2021-01-05 15:10:52 -0500

    added param defs

--- valid-palindrome.py
+++ valid-palindrome.py
@@ -1,21 +1,20 @@
-class Solution:
-    def isPalindrome(self, s: str) -> bool:
-        """
-        :s: str
-        :rVal: bool
-        """
+def isPalindrome(s):
+    """
+    :s: str
+    :rVal: bool
+    """
 
-        s = ''.join(i for i in s if i.isalnum()).lower()
-        return s == s[::-1]
+    s = ''.join(i for i in s if i.isalnum()).lower()
+    return s == s[::-1]
 
-        # INITIAL:
-        # s = ''.join(i for i in s if i.isalnum()).lower()
-        # print(s)
-        # i, j = 0, len(s) - 1
-        # while i <= j:
-        #     if s[i] != s[j]:
-        #         return False
-        #     i += 1
-        #     j -= 1
-            
-        # return True
+    # INITIAL:
+    # s = ''.join(i for i in s if i.isalnum()).lower()
+    # print(s)
+    # i, j = 0, len(s) - 1
+    # while i <= j:
+    #     if s[i] != s[j]:
+    #         return False
+    #     i += 1
+    #     j -= 1
+        
+    # return True
