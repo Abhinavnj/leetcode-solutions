39d30397c78f3905d54952159c4b1fa6a30565d5
Abhinav Sirohi <abhinavnj@gmail.com>
2021-01-05 18:10:39 -0500

    blind75 string #5

---
+++ group-anagrams.py
@@ -0,0 +1,22 @@
+def groupAnagrams(strs):
+    """
+    strs: List[str]
+    rVal: List[List[str]]
+    T: O(n)
+    """
+
+    # Values of 'groups' are the lists of words that are anagrams
+    # corresponding to the alphabetically sorted version of each word.
+    groups = {}
+    
+    for w in strs:
+        # Set 'key' equal to the sorted version of the string
+        key = ''.join(sorted(w))
+        # Set the value at this key as the words that match it when
+        # they are sorted. The words are stored in a list, which is
+        # the value.
+        groups[key] = groups.get(key, []) + [w]
+    
+    # The values of the dictionary are stored in a list, which is a
+    # list of lists containing the anagram groups in this case.
+    return groups.values()
