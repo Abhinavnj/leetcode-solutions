b8f2e4ec30f9aff8dcc85f874965cab8e4939185
Abhinav Sirohi <abhinavnj@gmail.com>
2021-03-15 01:00:30 -0400

    comments

--- best-time-to-buy-or-sell-stock.py
+++ best-time-to-buy-or-sell-stock.py
@@ -12,8 +12,17 @@ def maxProfit(prices):
     
     for i in range(1, len(prices)):
         if prices[i] < minPrice:
+            # Does not change the maxProfit immediately.
+            # Finds mins to compare the next values with.
             minPrice = prices[i]
             continue
+
+        # Check differences starting from the current min until the next min
+        # is encountered.
+        # Values to the right of the leftmost min keep being checked
+        # and the min is reset when another min is found and the rest of
+        # the values are compared.
+        # Only changes maxProfit once a greater profit is found.
         maxProfit = max(maxProfit, prices[i] - minPrice)
     
     return maxProfit
